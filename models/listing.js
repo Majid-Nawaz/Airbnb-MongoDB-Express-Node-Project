b2cdbbdd112401370c6187d1ebf444eb5ba76c7b
Majid-Nawaz <2024473@student.cct.ie>
2026-08-20 04:56:17 +0100

    Styling the templates
    Continuing Phase-1: Styling the ejs templates using bootstrap and css

--- models/listing.js
+++ models/listing.js
@@ -11,8 +11,8 @@ const listingSchema = new mongoose.Schema({ //use new here as we create new inst
     image:{
         filename: {type: String},
         url : {type: String,
-        default: "A URL",
-        set: (value)=>value ==="" ? "A URL": value,
+        default: "https://plus.unsplash.com/premium_photo-1661908377130-772731de98f6?q=80&w=2012&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        set: (value)=>value ==="" ? "https://plus.unsplash.com/premium_photo-1661908377130-772731de98f6?q=80&w=2012&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D": value,
         }
     },
     price:{
